85699157769a743df87c358708af58d41f95bef6
wwq <wwquen3568@naver.com>
2023-03-29 11:17:47 +0900

    refactor: 클라-서버 통신 방법 구체화

--- main/test_client.py
+++ main/test_client.py
@@ -1,15 +1,22 @@
 #client.py
-
 from socket import *
+from _thread import *
+## _thread 모듈로 msg 송신동작과 수신 동작을 각개 다른 스레드가 처리
+## 병렬 처리
 
 clientSock = socket(AF_INET, SOCK_STREAM)
 clientSock.connect(('203.252.240.40', 8080)) 
 
 
-print('연결 확인 됐습니다.')
-clientSock.send('I am a client'.encode('utf-8'))
+print('서버와 연결 확인 됐습니다.')
 
-print('메시지를 전송했습니다.')
 
-data = clientSock.recv(1024)
-print('받은 데이터 : ', data.decode('utf-8'))
+while True:
+    
+    
+    clientSock.send('I am a client'.encode('utf-8'))
+
+    print('메시지를 전송했습니다.')
+
+    msg = clientSock.recv(1024)
+    print('받은 데이터 : ', msg.decode('utf-8'))
--- main/test_server.py
+++ main/test_server.py
@@ -1,24 +1,43 @@
 #server.py
 from socket import *
+import time
 
 serverSock = socket(AF_INET, SOCK_STREAM) #두가지인자는 어드레스 패밀리, 소켓 타입 
 serverSock.bind(('', 8080)) #서버 소켓에서는 bind해줘야함, 
 # ''이므로 8080번 포트에서 모든 인터페이스에게 연결하도록 한다.
 
+print("대기중")
 serverSock.listen(1) #서버소켓에서만 쓰임,해당 소켓이 총 몇개의 동시접속까지를 허용수 인자
 
 connectionSock, addr = serverSock.accept() 
 print(str(addr),'에서 접속이 확인되었습니다.')
 
-
 while True :
-    try :
-        data = connectionSock.recv(1024)
-        print('받은 데이터 : ', data.decode('utf-8'))
+    data = connectionSock.recv(1024).decode('utf-8')
+    
+    if data == '종료':
+        break
+    
+    """
+    # if-elif로 해서 작업 분류하면될듯
+    ex)
+    
+    elif data.find('읽어줘'):
+        OCR 작업수행
+    
+    elif data.find('저거 알려줘'):
+        손끝점 작업 ㅜ=수행
+    
+    elif 등등
+        ...
+    """
+    
+    print(data + '에 대한 작업 처리 중')
+    time.sleep(1.5) # 작업 소요시간 1.5초
+    print(data + '에 대한 처리 완료')
+    
+    text = (data+"처리 결과값").encode('utf-8')
+    
+    connectionSock.send(text)
 
-        text=input("입력하세요 : ")
-
-        connectionSock.send(text.encode('utf-8'))
-        print('메시지를 보냈습니다.')
-    except :
-        pass
+serverSock.close()
